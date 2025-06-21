7de3f7da27470c0fdbb337f20daedb5ab00afa61
Nileath <nileath@proton.me>
2025-06-21 15:27:42 -0400

    Fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { DEFAULT_LOCALE_SETTING, LOCALES_SETTING } from './src/locales';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://nileath.dev',
-  base: '/',
+  base: '/nileath.dev/',
 	i18n: {
     defaultLocale: DEFAULT_LOCALE_SETTING,
     locales: Object.keys(LOCALES_SETTING),
